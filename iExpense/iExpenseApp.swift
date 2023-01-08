64288e8d76cbc5ebf79866eed6aeca0f360c56b4
Faizaan Khan <faizaankhan24@gmail.com>
2023-01-08 14:36:32 +0530

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Faizaan Khan on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
