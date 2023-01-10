151e7301a2a5568124ff5dda7a7e737b35ae7e02
Faizaan Khan <faizaankhan24@gmail.com>
2023-01-10 13:01:41 +0530

    Adding the logic

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,16 +7,93 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ExpenseView: View{
+    var expense : ExpenseItem
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack{
+            VStack(alignment: .leading) {
+                Text(expense.name)
+                    .font(.headline)
+                    .foregroundColor(getTextColor(amount: expense.amount))
+                Text(expense.type)
+            }
+            
+            Spacer()
+            
+            Text(expense.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+//                            .foregroundColor(textColor(amount: item.amount))
         }
-        .padding()
     }
+    
+    func getTextColor(amount: Double) -> Color{
+        if amount < 10 {
+            return Color.green
+        }
+        else if amount > 100 {
+            return Color.red
+        }
+        else {
+            return Color.orange
+        }
+    }
+}
+
+struct ContentView: View {
+    @State private var showingAddExpense = false
+    
+    @StateObject var expenses = Expenses()
+    
+    var body: some View {
+        NavigationView{
+            List{
+                Section("Personal"){
+                    ForEach(expenses.items){item in
+                        if(item.type == "Personal"){
+                            ExpenseView(expense: item)
+                        }
+                    }
+                    .onDelete(perform: removeItems)
+                }
+                
+                Section("Business"){
+                    ForEach(expenses.items){item in
+                        if(item.type == "Business"){
+                            ExpenseView(expense: item)
+                        }
+                    }
+                    .onDelete(perform: removeItems)
+                }
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button {
+                    showingAddExpense = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+            .sheet(isPresented: $showingAddExpense){
+                AddView(expenses: expenses)
+            }
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet){
+        expenses.items.remove(atOffsets: offsets)
+    }
+    
+//    func getTextColor(amount: Double) -> Color{
+//        if amount < 10 {
+//            return Color.green
+//        }
+//        else if amount > 100 {
+//            return Color.red
+//        }
+//        else {
+//            return Color.orange
+//        }
+//    }
 }
 
 struct ContentView_Previews: PreviewProvider {
--- iExpense/Expenses.swift
+++ iExpense/Expenses.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]() {
+        didSet {
+            let encoder = JSONEncoder()
+            
+            if let encodedData = try? encoder.encode(items){
+                UserDefaults.standard.set(encodedData, forKey: "Items")
+            }
+        }
+    }
+    
+    init() {
+        let decoder = JSONDecoder()
+        
+        if let savedData = UserDefaults.standard.data(forKey: "Items"){
+            if let decodedItems = try? decoder.decode([ExpenseItem].self, from: savedData){
+                items = decodedItems
+                return
+            }
+        }
+        
+        items = []
+    }
+}
